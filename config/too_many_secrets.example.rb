67542ada6a2574c1e3a2c144ef9377981152a4da
Chris Radcliff <chris.radcliff@gmail.com>
2013-06-27 14:32:35 -0700

    Removed DynamoDB variables from configuration.

--- config/too_many_secrets.example.rb
+++ config/too_many_secrets.example.rb
@@ -3,17 +3,6 @@
 
 ENV['RAILS_COOKIE_TOKEN'] = "at least 30 characters and all random"
 
-# Dynamoid data storage configuration
-#
-# See the Dynamoid documentation (https://github.com/Veraticus/Dynamoid)
-# or AWS documentation (https://github.com/aws/aws-sdk-ruby)
-# for details on where to get these keys.
-ENV['AWS_ACCESS_KEY_ID'] = 'key for DynamoDB on AWS'
-ENV['AWS_SECRET_ACCESS_KEY'] = 'secret key for DynamoDB on AWS'
-ENV['AWS_DYNAMO_DB_ENDPOINT'] = 'endpoint URL for your DynamoDB'
-# Use a namespace to separate your estuary tables from other DynamoDB tables
-ENV['DYNAMO_DB_NAMESPACE'] = 'my_own_estuary'
-
 # Social media API keys
 #
 ENV['TWITTER_CLIENT_KEY'] = "XXXXXXXX"

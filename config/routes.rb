f64dccfcc9ff37361c56e8a12ef42b7e09250b1d
Chris Radcliff <chris.radcliff@gmail.com>
2013-07-01 12:29:57 -0700

    Show the browse interface by default.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Estuary::Application.routes.draw do
 
+# The root (/) goes straight to browsing for now. 
+# Update this to offer an overview page instead.
+root :to => 'posts#show'
+
 get 'posts' => 'posts#show'
 
 end

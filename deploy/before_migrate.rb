38de8d51ae2272ee57394454032f9c505a0d6334
Andrew Hollingsworth <adh@techopsguru.com>
2013-07-23 10:52:50 -0700

    add hack to create shared assets directory if it does not exist

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,15 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
  
 Chef::Log.info("Symlinking #{release_path}/public/assets to #{new_resource.deploy_to}/shared/assets")
+
+# shared assets hack for opsworks -adh
+shared_assets = "#{new_resource.deploy_to}/shared/assets"
+if File.exists?(shared_assets) && File.directory?(shared_assets)
+    puts "#{shared_assets} directory exists"
+else
+    puts "creating #{shared_assets} directory..."
+    `mkdir -p #{shared_assets}`
+end
  
 link "#{release_path}/public/assets" do
   to "#{new_resource.deploy_to}/shared/assets"
